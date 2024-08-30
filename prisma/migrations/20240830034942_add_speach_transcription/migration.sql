ab711a891fe13011cad7a880039dfccde712fe82
Nathan Vale <hi@nathanvale.com>
2024-08-31 07:07:07 +1000

    implement open ai speach to text spike

---
+++ prisma/migrations/20240830034942_add_speach_transcription/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "SpeechTranscription" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "transcription" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
